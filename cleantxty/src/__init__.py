dcffda9b16a525e59efdc7692d97f83860434dca
aditya0072001 <tripathi.aditya1660@gmail.com>
2022-11-30 23:24:05 +0530

    fixing nltk issues

--- cleantxty/src/__init__.py
+++ cleantxty/src/__init__.py
@@ -1,7 +1,9 @@
 """__init__"""
 from .cleantxty import *
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __maintainer__ = 'Tripathi Aditya Prakash'
 
 __all__ = ['clean', 'clean_words','remove_link','remove_extra_white_space','lower_text','upper_text','remove_stopwords','remove_digits','remove_punctuations','custom_regex','stem_text']
+
+__str__ = "Cleaning text library for removing and formating text based on criterias provided"
