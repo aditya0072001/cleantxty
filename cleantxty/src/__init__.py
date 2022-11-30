ccdee299dd9964b3af704f99cd39b807b6b5bd0d
aditya0072001 <tripathi.aditya1660@gmail.com>
2022-11-30 23:18:35 +0530

    fixes

---
+++ cleantxty/src/__init__.py
@@ -0,0 +1,7 @@
+"""__init__"""
+from .cleantxty import *
+
+__version__ = '0.0.3'
+__maintainer__ = 'Tripathi Aditya Prakash'
+
+__all__ = ['clean', 'clean_words','remove_link','remove_extra_white_space','lower_text','upper_text','remove_stopwords','remove_digits','remove_punctuations','custom_regex','stem_text']
